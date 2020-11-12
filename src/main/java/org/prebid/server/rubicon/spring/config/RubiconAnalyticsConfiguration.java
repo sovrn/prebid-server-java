d87eabb42cccc4c89cdbef75566cf66783c0ec07
Andrii Braslavskyi <abraslavskyi@rubiconproject.com>
2020-11-12 14:13:01 +0200

    Move version logic (#502)

--- src/main/java/org/prebid/server/rubicon/spring/config/RubiconAnalyticsConfiguration.java
+++ src/main/java/org/prebid/server/rubicon/spring/config/RubiconAnalyticsConfiguration.java
@@ -1,8 +1,6 @@
 package org.prebid.server.rubicon.spring.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.prebid.server.bidder.BidderCatalog;
@@ -12,7 +10,7 @@ import org.prebid.server.json.JacksonMapper;
 import org.prebid.server.rubicon.analytics.RubiconAnalyticsModule;
 import org.prebid.server.rubicon.audit.UidsAuditCookieService;
 import org.prebid.server.util.HttpUtil;
-import org.prebid.server.util.ResourceUtil;
+import org.prebid.server.util.VersionInfo;
 import org.prebid.server.vertx.http.HttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -24,23 +22,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Configuration
 @ConditionalOnProperty(prefix = "analytics.rp", name = "enabled", havingValue = "true")
 public class RubiconAnalyticsConfiguration {
-
-    private static final Logger logger = LoggerFactory.getLogger(RubiconAnalyticsConfiguration.class);
-
-    private static final String VERSION_UNDEFINED = "undefined";
 
     @Bean
     @Primary
     RubiconAnalyticsModule rubiconAnalyticsModule(@Value("${external-url}") String externalUrl,
                                                   @Value("${datacenter-region}") String dataCenterRegion,
                                                   RubiconAnalyticsModuleProperties properties,
+                                                  VersionInfo versionInfo,
                                                   BidderCatalog bidderCatalog,
                                                   UidsCookieService uidsCookieService,
                                                   UidsAuditCookieService uidsAuditCookieService,
@@ -51,7 +43,7 @@ public class RubiconAnalyticsConfiguration {
         return new RubiconAnalyticsModule(
                 properties.getHostUrl(),
                 properties.getSamplingFactor(),
-                pbsVersion("git-revision.json", mapper),
+                versionInfo.getVersion(),
                 HttpUtil.getDomainFromUrl(externalUrl),
                 dataCenterRegion,
                 bidderCatalog,
@@ -60,30 +52,6 @@ public class RubiconAnalyticsConfiguration {
                 currencyConversionService,
                 httpClient,
                 mapper);
-    }
-
-    private static String pbsVersion(String gitRevisionPath, JacksonMapper mapper) {
-        final GitRevision gitRevision = readGitRevision(mapper, gitRevisionPath);
-        final String buildVersion = gitRevision.getBuildVersion();
-        final String extractedVersion = buildVersion != null ? extractVersion(buildVersion) : null;
-
-        return extractedVersion != null ? extractedVersion : VERSION_UNDEFINED;
-    }
-
-    private static GitRevision readGitRevision(JacksonMapper mapper, String gitRevisionPath) {
-        try {
-            return mapper.mapper().readValue(ResourceUtil.readFromClasspath(gitRevisionPath), GitRevision.class);
-        } catch (IllegalArgumentException | IOException e) {
-            logger.warn("Was not able to read revision file {0}. Reason: {1}", gitRevisionPath, e.getMessage());
-            return GitRevision.empty();
-        }
-    }
-
-    private static String extractVersion(String buildVersion) {
-        final Pattern versionPattern = Pattern.compile("\\d+\\.\\d+\\.\\d");
-        final Matcher versionMatcher = versionPattern.matcher(buildVersion);
-
-        return versionMatcher.lookingAt() ? versionMatcher.group() : null;
     }
 
     @lombok.Value(staticConstructor = "of")
